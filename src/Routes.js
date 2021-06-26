ed09542fb37b3cfd619b3a9be90fcc23de6246de
Vinayak Kuradia <vkuradia87@gmail.com>
2021-06-27 01:46:13 +0530

    Exported site module standalone

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router";
-import Home from "./pages/site/home";
-import About from "./pages/site/about";
-import Login from "./pages/site/login";
+import Site from "./pages/site";
 
 export default (
 	<Switch>
-		<Route exact path="/" component={Home} />
-		<Route exact path="/about" component={About} />
-		<Route exact path="/login" component={Login} />
+		<Route exact path="/" component={Site.Home} />
+		<Route exact path="/about" component={Site.About} />
+		<Route exact path="/login" component={Site.Login} />
 		<Route exact path="/p/user/create/" component={undefined} />
 		<Route exact path="/p/user/update/:username/" component={undefined} />
 		<Route exact path="/p/user/:username/" component={undefined} />
---
+++ src/pages/site/index.js
@@ -0,0 +1,7 @@
+import Home from "./home";
+import About from "./about";
+import Login from "./login";
+
+const Site = { Home, About, Login };
+
+export default Site;
